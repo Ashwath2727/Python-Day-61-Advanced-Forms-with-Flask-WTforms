23a0deae20333244104448a8f115e14775554634
Ashwath S <miniash123@outlook.com>
2025-12-07 01:08:38 +0530

    added bootstrap flask logic

---
+++ bootstrap_flask_main.py
@@ -0,0 +1,44 @@
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
+
+from my_form import MyForm
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+app.secret_key = "mysecretkey3127"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = MyForm()
+    # print(form.email.label)
+    if form.validate_on_submit():
+        print(form.email.data)
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
